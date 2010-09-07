ff34a8fcbbc36dfe4d35151c42176a5dbc170c59
Sridhar Ratnakumar <me@srid.name>
2010-09-06 21:59:17 -0700

    pep8'fy readability.py
    Used "%APPDATA%\Python\Scripts\pep8 %F" in Komodo

--- src/readability.py
+++ src/readability.py
@@ -1,17 +1,20 @@
 # From http://github.com/scyclops/Readable-Feeds/blob/master/readability/hn.py
 #    This file originally written by Nirmal Patel (http://nirmalpatel.com/).
-#    Generic feed and Google App Engine support added by Andrew Trusty (http://andrewtrusty.com/).
 # License: GPL
 
+import os
 import sys
-import urllib, re, os, urlparse
-import HTMLParser, feedparser
-from BeautifulSoup import BeautifulSoup
-
+import urllib
+import urlparse
+import re
+import HTMLParser
 import rfc822
 from datetime import datetime
 from pickle import dumps, loads
 import time
+
+import feedparser
+from BeautifulSoup import BeautifulSoup
 
 
 NEGATIVE    = re.compile("comment|meta|footer|footnote|foot")
@@ -20,38 +23,37 @@ PUNCTUATION = re.compile("""[!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~]""")
 
 
 def grabContent(link, html):
-    
     replaceBrs = re.compile("<br */? *>[ \r\n]*<br */? *>")
     html = re.sub(replaceBrs, "</p><p>", html)
-    
+
     try:
         soup = BeautifulSoup(html)
     except HTMLParser.HTMLParseError:
         return u""
-    
+
     # REMOVE SCRIPTS
     for s in soup.findAll("script"):
         s.extract()
-    
+
     allParagraphs = soup.findAll("p")
-    topParent     = None
-    
+    topParent = None
+
     parents = []
     for paragraph in allParagraphs:
-        
+
         parent = paragraph.parent
-        
+
         if (parent not in parents):
             parents.append(parent)
             parent.score = 0
-            
-            if (parent.has_key("class")):
+
+            if ("class" in parent):
                 if (NEGATIVE.match(parent["class"])):
                     parent.score -= 50
                 if (POSITIVE.match(parent["class"])):
                     parent.score += 25
-                    
-            if (parent.has_key("id")):
+
+            if ("id" in parent):
                 if (NEGATIVE.match(parent["id"])):
                     parent.score -= 50
                 if (POSITIVE.match(parent["id"])):
@@ -59,22 +61,23 @@ def grabContent(link, html):
 
         if (parent.score == None):
             parent.score = 0
-        
-        innerText = paragraph.renderContents() #"".join(paragraph.findAll(text=True))
+
+        innerText = paragraph.renderContents(
+            )  # "".join(paragraph.findAll(text=True))
         if (len(innerText) > 10):
             parent.score += 1
-            
+
         parent.score += innerText.count(",")
-        
+
     for parent in parents:
         if ((not topParent) or (parent.score > topParent.score)):
             topParent = parent
 
     if (not topParent):
         return u""
-            
+
     # REMOVE LINK'D STYLES
-    styleLinks = soup.findAll("link", attrs={"type" : "text/css"})
+    styleLinks = soup.findAll("link", attrs={"type": "text/css"})
     for s in styleLinks:
         s.extract()
 
@@ -86,28 +89,28 @@ def grabContent(link, html):
     for ele in topParent.findAll(True):
         del(ele['style'])
         del(ele['class'])
-        
-    killDivs(topParent)
-    clean(topParent, "form")
-    clean(topParent, "object")
-    clean(topParent, "iframe")
-    
-    fixLinks(topParent, link)
-    
-    return topParent.renderContents().decode('utf-8')
-    
 
-def fixLinks(parent, link):
+    _killDivs(topParent)
+    _clean(topParent, "form")
+    _clean(topParent, "object")
+    _clean(topParent, "iframe")
+
+    _fixLinks(topParent, link)
+
+    return topParent.renderContents().decode('utf-8')
+
+
+def _fixLinks(parent, link):
     tags = parent.findAll(True)
-    
+
     for t in tags:
-        if (t.has_key("href")):
+        if "href" in t:
             t["href"] = urlparse.urljoin(link, t["href"])
-        if (t.has_key("src")):
+        if "src" in t:
             t["src"] = urlparse.urljoin(link, t["src"])
 
 
-def clean(top, tag, minWords=10000):
+def _clean(top, tag, minWords=10000):
     tags = top.findAll(tag)
 
     for t in tags:
@@ -115,8 +118,7 @@ def clean(top, tag, minWords=10000):
             t.extract()
 
 
-def killDivs(parent):
-    
+def _killDivs(parent):
     divs = parent.findAll("div")
     for d in divs:
         p     = len(d.findAll("p"))
@@ -126,12 +128,12 @@ def killDivs(parent):
         embed = len(d.findAll("embed"))
         pre   = len(d.findAll("pre"))
         code  = len(d.findAll("code"))
-    
+
         if (d.renderContents().count(",") < 10):
             if ((pre == 0) and (code == 0)):
-                if ((img > p ) or (li > p) or (a > p) or (p == 0) or (embed > 0)):
+                if (img > p) or (li > p) or (a > p) or (p == 0) or (embed > 0):
                     d.extract()
-                    
+
 
 def main():
     url = sys.argv[1]
