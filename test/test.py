78809a1d70ded3803b5c4ada8c67dfa72a02e73a
Sridhar Ratnakumar <me@srid.name>
2010-09-15 23:41:54 -0700

    test: write html as temp file if a test fails

--- test/test.py
+++ test/test.py
@@ -1,6 +1,8 @@
+import os
 from os import path
 from glob import glob
 import difflib
+import tempfile
 
 from readability import readable
 
@@ -10,9 +12,28 @@ def test_fixtures():
         print('url=%s' % url)
         
         actual_html = readable(url)
+        
+        # Write the HTML for late diagnosis
+        _, actual_html_fn = tempfile.mkstemp('readabilitytest')
+        os.close(_)
+        _, expected_html_fn = tempfile.mkstemp('readabilitytest')
+        os.close(_)
+        with open(actual_html_fn, 'w') as f:
+            f.write(actual_html)
+        with open(expected_html_fn, 'w') as f:
+            f.write(expected_html)
+        
         diff = list(difflib.context_diff(
             actual_html.splitlines(), expected_html.splitlines()))
-        assert not diff, '\n'.join(diff)
+        diff_string = '\n'.join(diff)
+        assert not diff, (
+            'readable version differs; diff:\n{0}\n'
+            'expected: {1}\n'
+            'actual: {2}').format(
+            diff_string,
+            expected_html_fn,
+            actual_html_fn,
+        )
         
     fixtures_dir = path.join(path.dirname(__file__), 'fixtures')
     for urlf in glob(path.join(fixtures_dir, '*.url')):
